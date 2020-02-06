f5273940b25719ed23c152db441962ec0775a5f5
gaoxin <gao_xin@suixingpay.com>
2020-02-06 18:08:28 +0800

    你好

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(Center(
-    child: Text('你好',textDirection:TextDirection.ltr,style: TextStyle(color: Colors.green,fontSize: 20.0),)
+    child:  Text('你好',textDirection:TextDirection.ltr,style: TextStyle(color: Colors.green,fontSize: 20.0),)
   ));
 }
