6943ab355889a570fde42ac5027f6e923ce8e30d
gaoxin <gao_xin@suixingpay.com>
2020-02-08 19:00:01 +0800

    底部导航的实现

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,12 @@ import 'package:flutter/material.dart';
 
 void main () => runApp(MaterialApp(
   title: 'hello world',
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('flutter App'),
-    ),
-    body: Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: FormValue(),
-    ),
+  theme:ThemeData(
+    primaryColor: Colors.green,
+    highlightColor: Colors.transparent,
+    splashColor: Colors.transparent
   ),
+  home: FormValue()
 ));
 
 
@@ -24,69 +21,38 @@ class FormValue extends StatefulWidget {
 }
 
 class FormValueState extends State<FormValue> {
-  String userName;
-  String possworld;
-
-  final registerFormKey = GlobalKey<FormState>();
-
-
+  var _count = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Form(
-         key: registerFormKey,
-         child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.people),
-                  labelText: '用户名'
-                ),
-                onSaved: (value){
-                   this.userName = value;
-                },
-                validator: (value){
-                  if(value == null || value.length == 0){
-                    return '请输入用户名';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                obscureText:true,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.lock),
-                  labelText: '密码'
-                ),
-                onSaved: (value){
-                   this.possworld = value;
-                },
-                validator: (value){
-                  if(value == null || value.length == 0){
-                    return '请输入密码';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 10.0,),
-              Container(
-                width: double.infinity,
-                color: Colors.orange,
-                height: 50.0,
-                child: RaisedButton(
-                  color: Colors.orange,
-                  child: Text('注册',style: TextStyle(fontSize: 20.0,color: Colors.white),),
-                  onPressed: (){
-                    registerFormKey.currentState.save();
-                    registerFormKey.currentState.validate();
-                    print('$userName $possworld');
-                  },
-                ),
-              )
-           ],
-         ),
-       ),
-    );
+    return Scaffold(
+    appBar: AppBar(
+      title: Text('flutter App'),
+    ),
+    bottomNavigationBar: BottomNavigationBar(
+      iconSize: 36,
+      selectedFontSize: 24,
+      unselectedFontSize: 20,
+      currentIndex: _count,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          title: Text('首页')
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.pages),
+          title: Text('详情')
+        )
+      ],
+      onTap: (int value){
+        setState(() {
+          _count = value;
+        });
+      },
+    ),
+    body: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Text('2222')
+    ),
+  );
   }
 }
